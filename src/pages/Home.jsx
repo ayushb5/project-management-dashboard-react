a83f96b2fdd033818d616aa419d1ad1973108e3d
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-29 21:23:35 +0530

    feat: added Home page inside pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import ProjectList from "../Project/ProjectList"
+
+function Home() {
+    return (
+        <>
+            {/* Heading */}
+            <Header />
+            <ProjectList />
+        </>
+    )
+}
+
+export default Home
