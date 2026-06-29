a3a2f6e450a34e9046441a471fb54ad2e8725bd5
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-29 21:23:18 +0530

    feat: added reusable common component like header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <>
+            <h1 class="text-indigo">Project Flow</h1>
+            <p>Project Management Dashboard</p>
+        </>
+    )
+}
+
+export default Header
