e9b696a2965a6c0949ce16b557c85c28f1525ad8
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-30 20:22:53 +0530

    refractor: Added working Project List

--- src/Project/ProjectList.jsx
+++ src/Project/ProjectList.jsx
@@ -1,15 +1,48 @@
-import AddProject from "./AddProject"
+function ProjectList({ onSetPage, projects, onSetSelectedProject }) {
 
-function ProjectList() {
     return (
         <>
-            <hr />
             <div className="mx-100 d-flex justify-content-between align-items-center">
                 <h2>Projects</h2>
-                <button class="btn btn-green">+ New Project</button>
+                <button className="btn btn-green" onClick={() => onSetPage("add-project")}>+ New Project</button>
             </div>
-            <AddProject />
-
+            <div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {projects.length === 0 ? (
+                            <tr>
+                                <td colSpan={4} className="text-center">No Projects Added</td>
+                            </tr>
+                        ) : (
+                            projects.map((project, index) => (
+                                <tr key={project.id}>
+                                    <td>{index + 1}</td>
+                                    <td className="text-nowrap">{project.title}</td>
+                                    <td className="description">{project.description}</td>
+                                    <td>
+                                        <div className="d-flex justify-content-center" style={{ gap: "10px" }}>
+                                            <button className="btn-small" onClick={() => {
+                                                onSetSelectedProject(project);
+                                                onSetPage("add-task");
+                                            }}>View</button>
+                                            <button className="btn-small">Edit</button>
+                                            <button className="btn-small">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
