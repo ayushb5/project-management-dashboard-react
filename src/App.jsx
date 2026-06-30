8ae65de3160210c56115b653105a00addb4ec90f
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-06-30 20:21:27 +0530

    refractor: added navigation to add task with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Home from "./pages/Home"
+import AddProject from "./Project/AddProject";
+import AddTask from "./task/AddTask";
 function App() {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState(() => {
+    const savedProjects = JSON.parse(localStorage.getItem("projects"));
+    return savedProjects ? savedProjects : [];
+  });
   const [selectedProject, setSelectedProject] = useState(null);
   const [page, setPage] = useState("home")
+
+  useEffect(() => {
+    localStorage.setItem("projects", JSON.stringify(projects));
+  }, [projects]);
+
   return (
-    <div class="container">
-      {page === "home" && <Home />}
+    <div className="container">
+      {page === "home" && <Home onSetPage={setPage} projects={projects} onSetSelectedProject={setSelectedProject} />}
+      {page === "add-project" && <AddProject onSetPage={setPage} onSetProjects={setProjects} onSetSelectedProject={setSelectedProject} />}
+      {page === "add-task" && <AddTask onSetPage={setPage} project={selectedProject} projects={projects} onSetProjects={setProjects} onSetSelectedProject={setSelectedProject} />}
     </div>
   )
 }
