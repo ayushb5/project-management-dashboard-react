c65d31eb71e7b32488842c28c0393b3c76db1162
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-07-01 17:57:52 +0530

    refractor: add filtering, sorting, and task summary

--- src/task/TaskList.jsx
+++ src/task/TaskList.jsx
@@ -1,13 +1,58 @@
-function TaskList({ tasks }) {
+function TaskList({ tasks, allTasks, onEditTask, onDeleteTask, onFilterStatus, onSortBy, filterStatus, sortBy }) {
+
+    const getStatusWithBadge = (status) => {
+        if (status === "Pending") {
+            return <span className="badge badge-yellow">Pending</span>
+        } else if (status === "In Progress") {
+            return <span className="badge badge-blue">In Progress</span>
+        } else if (status === "Done") {
+            return <span className="badge badge-green">Done</span>
+        }
+    }
+
+    const counts = allTasks.reduce((acc, task) => {
+        if (task.status === "Pending") {
+            acc.pending++;
+        } else if (task.status === "In Progress") {
+            acc.inProgress++;
+        } else if (task.status === "Done") {
+            acc.done++;
+        }
+        return acc;
+    }, {
+        total: allTasks.length,
+        pending: 0,
+        inProgress: 0,
+        done: 0
+    })
+
     return (
         <>
-            <div className="table-container">
+            <div className="d-flex justify-content-end" style={{ "margin": "10px 0px", "gap": "10px", "flexDirection": "column" }}>
+                <div className="d-flex" style={{ "gap": "10px" }}>
+                    <span style={{ "fontWeight": "600" }}>Filter Status:</span>
+                    <select className="form-select" value={filterStatus} onChange={(e) => onFilterStatus(e.target.value)} className="form-select">
+                        <option value="All">All</option>
+                        <option value="Pending">Pending</option>
+                        <option value="In Progress">In Progress</option>
+                        <option value="Done">Done</option>
+                    </select>
+                </div>
+                <div className="d-flex" style={{ "gap": "10px" }}>
+                    <span style={{ "fontWeight": "600" }}>Sort By:</span>
+                    <select className="form-select" value={sortBy} onChange={(e) => onSortBy(e.target.value)} className="form-select">
+                        <option value="asc">Due Date (Ascending)</option>
+                        <option value="desc">Due Date (Descending)</option>
+                    </select>
+                </div>
+            </div >
+            <div className="table-container" style={{ "marginBottom": "30px" }}>
                 <table>
                     <thead>
                         <tr>
                             <th>#</th>
                             <th>Title</th>
-                            <th>Due Date</th>
+                            <th className="text-nowrap">Due Date</th>
                             <th>Status</th>
                             <th>Actions</th>
                         </tr>
@@ -22,12 +67,20 @@ function TaskList({ tasks }) {
                                 <tr key={task.id}>
                                     <td>{index + 1}</td>
                                     <td>{task.title}</td>
-                                    <td>{task.dueDate}</td>
-                                    <td>{task.status}</td>
+                                    <td className="text-nowrap">{task.dueDate}</td>
+                                    <td>{getStatusWithBadge(task.status)}</td>
                                     <td>
                                         <div className="d-flex justify-content-center" style={{ gap: "10px" }}>
-                                            <button className="btn-small">Edit</button>
-                                            <button className="btn-small">Delete</button>
+                                            <button className="btn-small" onClick={() => onEditTask(task)}>Edit</button>
+                                            <button className="btn-small" onClick={() => {
+                                                if (window.confirm("Are you sure, you want to delete this task?")) {
+                                                    onDeleteTask(task.id);
+                                                } else {
+                                                    return;
+                                                }
+                                            }}>
+                                                Delete
+                                            </button>
                                         </div>
                                     </td>
                                 </tr>
@@ -35,6 +88,16 @@ function TaskList({ tasks }) {
                         )}
                     </tbody>
                 </table>
+            </div>
+
+            <div className="taskSummary">
+                <h2>Task Summary</h2>
+                <p>
+                    {counts.total} {counts.total > 1 ? "Tasks" : "Task"} :{" "}
+                    {counts.done} Done,{" "}
+                    {counts.inProgress} In Progress,{" "}
+                    {counts.pending} Pending
+                </p>
             </div>
         </>
     )
